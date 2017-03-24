6271e8cc26a0bfd51eef2b5d1c21181adea9669a
Per <per.nordenbrink@bredband.net>
2017-03-24 09:21:33 +0100

    "Hello <e-mail>!" i navbaren har ersatts med "Hej <namn>!"

---
+++ LexiconLMS/Extensions/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Web;
+
+namespace LexiconLMS.Extensions
+{
+    public static class ExtensionMethods
+    {
+        public static string GetName(this IIdentity identity)
+        {
+            return ((ClaimsIdentity)identity).FindFirstValue("Namn");
+        }
+    }
+}
